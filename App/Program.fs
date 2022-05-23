e93fbfae874b533c256a07c12a00b5d106cb6e40
Ricardo Milan <ricardo@fluencypass.com>
2022-05-23 10:44:58 -0300

    menu

--- App/Program.fs
+++ App/Program.fs
@@ -1,6 +1,13 @@
 ﻿open HelloModule
 open InputModule
 
-let name = GetInput("Input: ")
+Display("Usando F#")
+Display("Selecione uma opção: ")
+Display("1: Hello World")
 
-SayHelloCase name true
+match GetInput("") with
+| "1" -> 
+    let name = GetInput("Input: ")
+    SayHelloCase name true
+| _ ->
+    Display("Opção inválida")
