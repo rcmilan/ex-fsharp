96c022cc1a53f988e47a4605004dc00c5c3a2700
Ricardo Milan <ricardo@fluencypass.com>
2022-05-24 08:47:54 -0300

    composição de tipos

---
+++ App/Types/TypeComposition.fs
@@ -0,0 +1,21 @@
+﻿module TypeComposition
+
+type PaycheckNum = int
+type CreditCardNum = string
+
+type CreditCardType = Visa | Mastercard // OR
+type CreditCardInfo = CreditCardType * CreditCardNum // AND
+
+type PaymentMethod =
+| Money // OR
+| Paycheck of PaycheckNum // OR
+| CreditCard of CreditCardInfo // OR
+
+type CurrencyValue = decimal
+type Currency = EUR | USD
+
+type Payment = {
+CurrencyValue : CurrencyValue
+Currency : Currency
+Method : PaymentMethod
+}
