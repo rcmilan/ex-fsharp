1129f66a775afcbfd61e98284a322d1b76028a2f
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-01-18 09:07:35 -0300

    union case

---
+++ App/UnionCases/unionCase.fs
@@ -0,0 +1,22 @@
+﻿module unionCase
+
+type RecordA = { a:int }
+type RecordB = { b:string; c:bool }
+type RecordC = { a:int; b:string; c:bool }
+
+type MyUnion =
+    | RecordA of int
+    | RecordB of string * bool
+    | RecordC of int * string * bool
+
+let x : MyUnion = RecordA(1)
+let y : MyUnion = RecordB("aaaa", true)
+let z : MyUnion = RecordC(2, "bbb", true)
+
+let unionList = [| x; y; z |]
+
+for a in unionList do
+    match a with
+    | RecordA(a1) -> printfn "%d" a1
+    | RecordB(b1, b2) -> printfn "%s" b1
+    | RecordC(c1, c2, c3) -> printfn "%d %s %b" c1 c2 c3
